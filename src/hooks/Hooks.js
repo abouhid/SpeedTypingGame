1b8d4660352d014e8fe00eb753c780d3c320f8dc
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-29 10:35:46 -0300

    game created

---
+++ src/hooks/Hooks.js
@@ -0,0 +1,52 @@
+import { useState, useEffect,useRef } from 'react'
+
+const Hooks = (time=10) => {
+    const [timer,setTimer]=useState(time)
+    const [startGameState, setStartGameState] = useState(false)
+    const [count,setCount]=useState(0)
+    const [text, setText] = useState("")
+    const boxRef = useRef(null)
+
+    const handleChange = e => {
+        const {value} = e.target
+        setText(value)
+    }
+    const wordCount = text => {
+        let arr = text.trim().split(" ")
+        let filteredArr = arr.filter(word=>word!=="")
+        return filteredArr.length        
+    }
+
+    const decrementTime = () => {
+        setTimer(prevTime=>prevTime-1)
+    }
+
+    const startGame = () => {    
+        setStartGameState(true)
+        setCount(0)
+        setText("")
+        boxRef.current.disabled = false   
+        boxRef.current.focus()
+    }
+
+    const endGame = () => {
+        setTimer(time)
+        setStartGameState(false)
+        setText("")
+        setCount(wordCount(text))
+
+    }
+
+    useEffect(()=> {
+        if (startGameState && timer > 0){
+            setTimeout(decrementTime, 1000)
+        } else if (timer===0){            
+            endGame()
+        }
+        
+    },[timer,startGameState])
+
+    return [timer,startGame,handleChange,count,text,startGameState,boxRef]
+}
+
+export default Hooks
