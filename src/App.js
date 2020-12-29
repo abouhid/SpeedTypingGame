dc99ddc3f6a026a933625166a86acec8a73ed8ad
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-12-29 11:17:36 -0300

    added linters

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.css';
-import Hooks from './hooks/Hooks'
+import Hooks from './hooks/Hooks';
 
 function App() {
-  const [time,startGame,handleChange,count,text,startGameState,boxRef] = Hooks(5)
+  const [time, startGame, handleChange, count, text, startGameState, boxRef] = Hooks();
   return (
     <div className="App">
       <h1>Word Typing Game</h1>
-      <textarea 
-      ref={boxRef} 
-      disabled={!startGameState}
+      <textarea
+        ref={boxRef}
+        disabled={!startGameState}
         onChange={handleChange}
         value={text}
       />
-      <h4>Time Remaining: {time} </h4>
-      <button  disabled={startGameState} onClick={startGame}> {count > 0 ? "Play again" : "Start"}</button>
-      <h4>Word Count: {count}</h4>
+      <h4>
+        Time Remaining:
+        {time}
+      </h4>
+      <button type="button" disabled={startGameState} onClick={startGame}>
+        {' '}
+        {count > 0 ? 'Play again' : 'Start'}
+      </button>
+      <h4>
+        Word Count:
+        {count}
+      </h4>
 
     </div>
   );
